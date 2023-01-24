f9a7c6c645b3ebbd4e1eba4abd0c602c808799f7
Amogh Chaubey <chaubeyamogh@gmail.com>
2023-01-23 23:28:09 -0500

    ok so how does HAVING work?!?

--- record_company.sql
+++ record_company.sql
@@ -49,5 +49,6 @@ GROUP BY band_id;
 SELECT b.name AS band_name, COUNT(a.id) 
 AS num_albums FROM bands AS b 
 LEFT JOIN albums AS a ON b.id = a.band_id
-GROUP BY b.id;
+GROUP BY b.id
+HAVING num_albums = 1;
 
