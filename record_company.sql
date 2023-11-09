b70247c689616a6fa65e9b65d6817e88eb81d847
Amogh Chaubey <chaubeyamogh@gmail.com>
2023-11-09 09:54:32 -0500

    lol

--- record_company.sql
+++ record_company.sql
@@ -1,55 +1,8 @@
-CREATE DATABASE record_company;
-USE record_company;
-CREATE TABLE bands (
-	id INT NOT NULL AUTO_INCREMENT,
-	name VARCHAR(255) NOT NULL,   
-    PRIMARY KEY (id)
-    );
-
 CREATE TABLE albums (
-	id INT NOT NULL AUTO_INCREMENT,
-    name VARCHAR(255) NOT NULL,
-    release_year INT, 
-    band_id INT NOT NULL,
-    PRIMARY KEY (id),
-    FOREIGN KEY(band_id) REFERENCES bands(id)
+  id INT NOT NULL AUTO_INCREMENT,
+  name VARCHAR(255) NOT NULL,
+  release_year INT,
+  band_id INT NOT NULL,
+  PRIMARY KEY (id),
+  FOREIGN KEY (band_id) REFERENCES bands(id)
 );
-
-INSERT INTO bands (name)
-VALUES ('Iron Maiden'); 
-INSERT into bands (name)
-VALUES ('Deuce'), ('Avenge Sevenfold'), ('Ankor'); 
-
-SELECT * FROM BANDS ORDER BY name;
-
-INSERT INTO albums (name, release_year, band_id)
-VALUES ('The Number of the Beast', 1985, 1), 
-	   ('Power Slave', 1984, 1),
-       ('Nightmare', 2018, 2),
-       ('Nightmare', 2010, 3),
-       ('Test Album', NULL, 3);
-
-SELECT * FROM albums
-WHERE name LIKE '%er%' OR band_id = 2;
-
-SELECT * from albums
-WHERE release_year = 1984 AND band_id = 1;
-
-SELECT * FROM albums
-WHERE release_year BETWEEN 2000 and 2018;
-
-SELECT * FROM bands
-LEFT JOIN albums ON bands.id = albums.band_id;
-
-SELECT AVG(release_year) FROM albums;
-
-SELECT band_id, COUNT(band_id) FROM albums
-GROUP BY band_id;
-
-SELECT b.name AS band_name, COUNT(a.id) 
-AS num_albums FROM bands AS b 
-LEFT JOIN albums AS a ON b.id = a.band_id
-WHERE b.name = "Deuce"
-GROUP BY b.id
-HAVING num_albums = 1;
-
